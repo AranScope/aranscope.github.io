90dd84dee879840861c35ece4ed1fdd939ec935c
Aran Long <aranlong@live.co.uk>
2017-04-23 20:12:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# install caddy
+curl https://getcaddy.com | bash
+
+# install node and npm
+sudo apt-get install nodejs
+sudo apt-get install npm
+
 # install docker
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
