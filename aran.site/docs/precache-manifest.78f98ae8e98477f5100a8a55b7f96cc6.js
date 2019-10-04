6c257d6752e49ed4eff0975d1599c854ac3a7934
aranscope <aranlong@live.co.uk>
2019-10-04 19:53:15 +0100

    setup for GitHub pages

---
+++ aran.site/docs/precache-manifest.78f98ae8e98477f5100a8a55b7f96cc6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46d286c02f805e0cce8e",
+    "url": "/static/js/main.46d286c0.chunk.js"
+  },
+  {
+    "revision": "985a1bb48a81e224e9c2",
+    "url": "/static/js/1.985a1bb4.chunk.js"
+  },
+  {
+    "revision": "cdbd5f8b5e3a372936bc71dcc3e0b807",
+    "url": "/index.html"
+  }
+];
