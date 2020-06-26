72c89db39ca3d6bef93301cee47d5594849df175
aranscope <aranlong@live.co.uk>
2020-06-26 16:52:07 +0100

    fix ci

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -181,7 +181,7 @@ export default ({data}) => (
                                             <div key={project.name}>
                                                 {
                                                     project.link ? (
-                                                        <a className="flex items-center text-xl font-bold font-sans tracking-tighter" target="_blank" rel="noreferrer" href={project.link}>{project.name} <svg className="ml-1 h-3 w-3 text-gray-500 inline-block" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"/></svg></a>
+                                                        <a className="flex items-center text-xl font-bold font-sans tracking-tighter" target="_blank" rel="noreferrer noopener" href={project.link}>{project.name} <svg className="ml-1 h-3 w-3 text-gray-500 inline-block" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.26 13a2 2 0 0 1 .01-2.01A3 3 0 0 0 9 5H5a3 3 0 0 0 0 6h.08a6.06 6.06 0 0 0 0 2H5A5 5 0 0 1 5 3h4a5 5 0 0 1 .26 10zm1.48-6a2 2 0 0 1-.01 2.01A3 3 0 0 0 11 15h4a3 3 0 0 0 0-6h-.08a6.06 6.06 0 0 0 0-2H15a5 5 0 0 1 0 10h-4a5 5 0 0 1-.26-10z"/></svg></a>
                                                     ) : <h3 className="text-xl font-bold font-sans tracking-tighter">{project.name}</h3>
                                                 }
 
